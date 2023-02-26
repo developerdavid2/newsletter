8a83c2328dc08ee23a55d577a4e4524acb2e0198
David5183 <jacobsdavid.dr@gmail.com>
2023-02-26 14:53:48 +0100

    added folders to repo

---
+++ app.js
@@ -0,0 +1,71 @@
+const { response } = require("express");
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/public/signup.html");
+});
+
+app.post("/", function (req, res) {
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us21.api.mailchimp.com/3.0/lists/27396b0085";
+
+  const options = {
+    method: "POST",
+    auth: "David5183:2e2350038c8c4ebdeddbd4a8ddbb7cb9-us21"
+  }
+
+  const request= https.request(url, options, function (response) {
+    if (response.statusCode===200){
+        res.sendFile(__dirname + "/public/success.html")
+    } else {
+        res.sendFile(__dirname + "/public/failure.html")
+    }
+
+    response.on("data", function (data) {
+      console.log(JSON.parse(data));
+    });});
+
+  request.write(jsonData);
+  request.end();
+});
+
+
+app.post("/failure", function (req, res) {
+  res.redirect("/");
+});
+
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Server is running on port 3000");
+});
+
+
+
+
